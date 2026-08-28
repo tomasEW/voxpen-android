5beba2cab97e42dd798e71dd697e4239e678637a
tomasEW <164850168+tomasEW@users.noreply.github.com>
2026-08-28 18:06:27 +0800

    build: bump app version to 1.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.voxpen.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.2.5"
+        versionCode = 11
+        versionName = "1.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
